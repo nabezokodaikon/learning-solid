ab647fa516a6cf852f006a18c95dcd19361c4a13
nabezokodaikon <nabezoko.daikon@gmail.com>
2022-06-01 01:24:57 +0900

    Add first code

--- hello-world/src/index.tsx
+++ hello-world/src/index.tsx
@@ -1,2 +1,10 @@
-import { render 
-} from 'solid-js/web';
+import { render } from 'solid-js/web';
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>Hello World!!!</div>
+  );
+};
+
+render(() => <App />, document.getElementById('root') as HTMLElement);
